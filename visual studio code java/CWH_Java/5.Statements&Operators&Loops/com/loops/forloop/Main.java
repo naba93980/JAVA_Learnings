701b786ee94b3d88ce5fe1c49d6a40deab447eb0
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-07-28 17:42:37 +0530

    updated on 28/7/22 at 5:45pm

--- visual studio code java/CWH_Java/5.Statements&Operators&Loops/com/loops/forloop/Main.java
+++ visual studio code java/CWH_Java/5.Statements&Operators&Loops/com/loops/forloop/Main.java
@@ -2,6 +2,9 @@ package com.loops.forloop;
 
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 10; i++ )
+        {
+            System.out.println(i);
+        }
     }
 }
