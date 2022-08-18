e227777f2ad01898d939fc388e0cfeed37839e86
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-18 17:04:21 +0530

    added default, private methods and other modifiers

--- visual_studio_code_java/CWH_Java/8.OOPS/interfaceinjava/Main.java
+++ visual_studio_code_java/CWH_Java/8.OOPS/interfaceinjava/Main.java
@@ -1,6 +1,7 @@
 package interfaceinjava;
 
 // all methods in interface are implicitly public
+// methods in interface can be default or static access specified
 
 interface Bicycle {
 
@@ -8,13 +9,22 @@ interface Bicycle {
 
     void speedUp();
 
+    private void color() {
+        System.out.println("red");
+    }
+
+    default void turnLightson() {
+        System.out.println("light are on...");
+        color();
+    }
+
     // only static methods can be defined in interface and subclass doesnot inherit
     // static methods of interfaces
     static void gearChange() {
         System.out.println("changing gear");
     }
 
-    // constant are final,public and static
+    // variable are final,public and static
     int a = 50;
 }
 
@@ -49,6 +59,10 @@ class AvonCycle extends Wheels implements Bicycle, Horn {
         System.out.println("blowing pee");
     }
 
+    // public void turnLightson() {
+    // System.out.println("light are on in avoncycle class...");
+    // }
+
     // void gearChange() {
     // System.out.println("changing gear from method in avoncycle");
     // }
@@ -59,23 +73,35 @@ public class Main {
 
         AvonCycle obj = new AvonCycle();
         obj.applyBrake();
-
+        obj.turnLightson();
         obj.noOfWheels(32);
 
         // System.out.println(obj.a); //ok
         // obj.gearChange(); // not ok if gearchange is not defined in class AvonCycle
         Bicycle.gearChange();
+
+        Bicycle kk = new AvonCycle();
+        kk.turnLightson();
     }
 }
 
 // interfaces are reference type(class is also basically reference type) where
-// all methods are abstract. (static methods can only have body)
+// all methods are abstract. (static and default methods can only have body)
 
 // we cant extend multiple abstract class but we can implement multiple
 // interfaces at a time.
 
-// All abstract, default, and static methods in an interface are implicitly
+// all abstract, default, and static methods in an interface are implicitly
 // public.
+
+// default method can be overridden
 
 // an interface can contain constant declarations. All constant values defined
 // in an interface are implicitly public , static , and final.
+
+// for methods in interface only public(available everywhere), private(available
+// within interface only), abstract(definition in subclass), default(definition
+// in interface, can be overriden in subclass), static(definition in interface
+// and can be accessed only with interface name) and
+// strictfp modifiers are available,
+// for variables final,public and static are available
