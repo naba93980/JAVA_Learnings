81df813cbaf372f55778211864ab510dfb2b64dc
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-10 00:07:22 +0530

    Primitive to String and reverse

---
+++ core_java_made_easy/Topics/WrapperClasses/PrimitiveAndString.java
@@ -0,0 +1,11 @@
+package WrapperClasses;
+
+public class PrimitiveAndString {
+    public static void main(String[] args) {
+        int a=100;
+        String b = Integer.toString(a);
+        System.out.println(b);
+        int c = Integer.parseInt(b);
+        System.out.println(c);
+    }
+}
