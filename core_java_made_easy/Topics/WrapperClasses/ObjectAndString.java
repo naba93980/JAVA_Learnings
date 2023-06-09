90e999a5cbfbc29324b5131109c7feb0c6dc09d6
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-10 00:23:16 +0530

    Integer object to String object

---
+++ core_java_made_easy/Topics/WrapperClasses/ObjectAndString.java
@@ -0,0 +1,10 @@
+package WrapperClasses;
+
+public class ObjectAndString {
+    public static void main(String[] args) {
+        int a=100;
+        Integer b = Integer.valueOf(a);     // converts to Integer object
+        String c = b.toString();                  // Integer object to String object
+        System.out.println(c);
+    }
+}
