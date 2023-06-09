b29a692e430a47aebc5b74a37509d75a3268cb24
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-09 23:59:10 +0530

    explicit casting ...bigger types getting assigned to smaller types

---
+++ core_java_made_easy/Topics/Types/ExplicitCast.java
@@ -0,0 +1,14 @@
+package Types;
+
+public class ExplicitCast {
+    public static void main(String[] args) {
+        int x = 100;
+        char y = (char) x;
+        System.out.println(y);
+
+        int a = 129 ;
+        byte b = (byte) a;
+        System.out.println(b);
+    }
+}
+ 
