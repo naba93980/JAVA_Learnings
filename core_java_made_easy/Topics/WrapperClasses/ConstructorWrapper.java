4fa887d7aeccedac7b47209ee644d0e31b63cbb7
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-10 13:55:10 +0530

    constructor wrapper

---
+++ core_java_made_easy/Topics/WrapperClasses/ConstructorWrapper.java
@@ -0,0 +1,9 @@
+package WrapperClasses;
+
+public class ConstructorWrapper {
+    public static void main(String[] args) {
+        String s="99";
+        Integer a=new Integer(s);
+        System.out.println(a);
+    }
+}
