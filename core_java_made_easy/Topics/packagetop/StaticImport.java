a43060ef2ae85effc419c7e31fab09f3510b2245
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-11 03:33:23 +0530

    static import of packages

---
+++ core_java_made_easy/Topics/packagetop/StaticImport.java
@@ -0,0 +1,10 @@
+package packagetop;
+
+
+import static java.lang.System.out;
+
+public class StaticImport {
+    public static void main(String[] args) {
+        out.println("My self Nabajyoti");
+    }
+}
