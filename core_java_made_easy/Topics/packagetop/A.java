831750911ac851d8bde78b1d76b78b1957fe3d6b
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-11 03:33:37 +0530

    packages in java

---
+++ core_java_made_easy/Topics/packagetop/A.java
@@ -0,0 +1,10 @@
+package packagetop;
+
+import packagetop.subpackage.B; // top level package needs to import subpackage
+
+public class A {
+    B b=new B();
+}
+
+
+// package name usually is reverse of company domain name
---
+++ core_java_made_easy/Topics/packagetop/subpackage/B.java
@@ -0,0 +1,7 @@
+package packagetop.subpackage;
+
+import packagetop.A;  // subpackage need to import top level package
+
+public class B {
+    A a=new A();
+}
