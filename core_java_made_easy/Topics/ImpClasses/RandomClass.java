edfbbeb5751c29e05ba8bbdf715078ceb74e2cf4
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-18 18:07:08 +0530

    Random class

---
+++ core_java_made_easy/Topics/ImpClasses/RandomClass.java
@@ -0,0 +1,13 @@
+package ImpClasses;
+
+import java.util.Random;
+
+public class RandomClass {
+    public static void main(String[] args) {
+        Random  r = new Random();
+        System.out.println(r.nextInt(100));
+        System.out.println(r.nextInt(100));
+        System.out.println(r.nextInt(100));
+       
+    }
+}
