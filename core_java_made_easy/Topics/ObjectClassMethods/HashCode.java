6b5609517b49a3f0a4e28a4f4bc552fc2d5777f0
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-25 23:55:11 +0530

    override hashCode()

---
+++ core_java_made_easy/Topics/ObjectClassMethods/HashCode.java
@@ -0,0 +1,52 @@
+package ObjectClassMethods;
+
+class Passenger {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        // final int prime = 31;
+        // int result = 1;
+        // result = prime * result + id;
+        // return result;
+        return this.id;
+    }
+
+}
+
+public class HashCode {
+    public static void main(String[] args) {
+        Passenger passenger=new Passenger();
+        passenger.setId(123);
+        passenger.setFirstName("Nabajyoti");
+        passenger.setLastName("Modak");
+        System.out.println(passenger.hashCode());
+    }
+}
