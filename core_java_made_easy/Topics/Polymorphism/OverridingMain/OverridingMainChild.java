142770e9fdae256c93a79dd03ab9836118035133
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-06-27 22:24:09 +0530

    main() can be overrided

---
+++ core_java_made_easy/Topics/Polymorphism/OverridingMain/OverridingMainChild.java
@@ -0,0 +1,10 @@
+package Polymorphism.OverridingMain;
+
+public class OverridingMainChild extends OverridingMainParent{
+
+    // this file runs even if psvm is not defined as psvm of parent class gets called
+
+    // public static void main(String[] args) {
+    //     System.out.println("inside child class main fn");
+    // }
+}
---
+++ core_java_made_easy/Topics/Polymorphism/OverridingMain/OverridingMainParent.java
@@ -0,0 +1,7 @@
+package Polymorphism.OverridingMain;
+
+public class OverridingMainParent {
+    public static void main(String[] args) {
+        System.out.println("inside parent class main fn");
+    }
+}
