d36a6322bc5f0c0aa8b2e711a22a56a2db411bc1
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-07-02 21:46:17 +0530

    Duration

---
+++ core_java_made_easy/Topics/DateTimeApi/DurationDemo.java
@@ -0,0 +1,27 @@
+package DateTimeApi;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class DurationDemo {
+    public static void main(String[] args) {
+
+        long sum = 0;
+
+        Instant start = Instant.now();
+        for (int i = 0; i < 100000000; i++) {
+            sum += i;
+        }
+        Instant end = Instant.now();
+        Duration timeNeeded = Duration.between(start, end);
+        System.out.println(timeNeeded.toMillis());
+
+        Duration d = Duration.of(5, ChronoUnit.DAYS);
+        System.out.println(d);
+        
+        Duration dH = Duration.ofHours(2);
+        System.out.println(dH);
+
+    }
+}
