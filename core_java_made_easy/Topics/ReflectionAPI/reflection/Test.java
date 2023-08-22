54e3044ceba7d0e5211ed9c540ad4c3301d49bbd
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-08-22 23:57:00 +0530

    invoking the constructors to create object

--- core_java_made_easy/Topics/ReflectionAPI/reflection/Test.java
+++ core_java_made_easy/Topics/ReflectionAPI/reflection/Test.java
@@ -1,20 +1,31 @@
 package ReflectionAPI.reflection;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class Test {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
         // Class<Calculator> calClass=Calculator.class; // This can also be used, but it is compile-time
-        Class<?> calClass = Class.forName(Calculator.class.getName());
+        Class<?> calClass = Class.forName(Calculator.class.getName()); // the Calculator.class object is stored in meta-space of jvm during class loading
         System.out.println(calClass.getName());
 
+        // gets all the constructors
         Constructor<?>[] constructors = calClass.getConstructors();
         System.out.println(Arrays.toString(constructors));
         Method[] methods = calClass.getDeclaredMethods();
         System.out.println(Arrays.toString(methods));
+
+        Constructor<?> constructor = calClass.getConstructor(); // gets the default constructor
+        Calculator calClassInstance = (Calculator) constructor.newInstance();
+        calClassInstance.setNum1(100);
+        System.out.println(calClassInstance.getNum1());
+
+        Constructor<?> constructor2 = calClass.getConstructor(int.class); // gets the parameterized constructor
+        Calculator calClassInstance2 = (Calculator) constructor2.newInstance(5);
+        System.out.println(calClassInstance2);
 
     }
 }
