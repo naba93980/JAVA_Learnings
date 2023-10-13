0f9e4ccd4a498bff162bf9eedf9594dc1730c0e4
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-10-14 01:50:11 +0530

    dynamic class loading

---
+++ core_java_made_easy/Topics/JVM/dynamicclassloading/User.java
@@ -0,0 +1,11 @@
+package JVM.dynamicclassloading;
+
+public class User<Integer> {
+
+    public String getName(){
+        return "naba";
+    }
+    public String getEmail(){
+        return "naba@gmai.com";
+    }
+}
