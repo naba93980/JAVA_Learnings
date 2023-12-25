3f23e697cd2e415cf3892d428eef3cf1cb30b1bd
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-12-26 03:17:35 +0530

    container type method reference

--- core_java_made_easy/Topics/Java8Features/Function/FunctionTest.java
+++ core_java_made_easy/Topics/Java8Features/Function/FunctionTest.java
@@ -6,5 +6,9 @@ public class FunctionTest {
     public static void main(String[] args) {
         Function<String, Integer> f = (s) -> s.length();    //like predicate but can return any type
         System.out.println(f.apply("hello"));
+
+        Function<String, Integer> f2 = String::length;      // ContainingType::methodName - reference to an instance method of an arbitrary object of a particular type
+        System.out.println(f2.apply("naba"));
     }
 }
+// map of js map, filter, reduce
